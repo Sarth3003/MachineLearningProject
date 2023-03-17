c67210996015b7f435b056dd1276bc6d845fa27a
sarthak <sarth3003@gmail.com>
2023-03-17 20:45:54 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -18,14 +18,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-
-if __name__ == "__main__":
-    try:
-        a =1/0
-        logging.info("Divde by zero")
-    except Exception as e:
-        raise CustomException(e,sys)
-              
-
-        
